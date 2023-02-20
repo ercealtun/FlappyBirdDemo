74895737f45fbea6d9a17d77ca8c5c941ad0e8e8
Erce Altun <ercealtn@gmail.com>
2023-02-20 11:59:28 +0300

    Initial commit

---
+++ Source/FlappyBirdDemo/FlappyBirdDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FlappyBirdDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLAPPYBIRDDEMO_API AFlappyBirdDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
